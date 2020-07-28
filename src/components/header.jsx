e09570a7ec943a87c149c725c687b471b18948f1
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-07-27 21:36:20 -0400

    created and styled input component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,7 @@ export default function Header(props) {
     return(
         <div className="Header__Block">
             <h1 className="Header__Title">Stocks Dashboard</h1>
+            <input className="Header__Search"/>
         </div>
     )
 }
