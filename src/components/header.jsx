009eaff9fdfe9bda770a76ce46c73e2188273369
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-07-27 21:31:17 -0400

    started adding some styling and the header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,1 +1,10 @@
 import React from 'react';
+import './header.css';
+
+export default function Header(props) {
+    return(
+        <div className="Header__Block">
+            <h1 className="Header__Title">Stocks Dashboard</h1>
+        </div>
+    )
+}
