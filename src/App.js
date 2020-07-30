c50c64fd7ff65b4e8445bc2149435109eaa01b6c
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-07-29 22:38:48 -0400

    managing state of input bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './components/header';
 import Body from './components/Body';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
     <div>
-      <Header/>
+      <Header search={search}/>
       <Body/>
     </div>
   )
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,7 +5,7 @@ export default function Header(props) {
     return(
         <div className="Header__Block">
             <h1 className="Header__Title">Stocks Dashboard</h1>
-            <input className="Header__Search"/>
+            <input className="Header__Search" value={props.search} onChange={} />
         </div>
     )
 }
