dceedd55b39825f7a6e6c3b4b2aad10d70a82d9f
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-07-29 22:48:03 -0400

    input change works

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Header from './components/header';
 import Body from './components/Body';
 import './App.css';
-import {useState} from 'react';
+
 
 function App() {
-  const [search, setSearch] = useState("");
   return (
     <div>
-      <Header search={search}/>
+      <Header/>
       <Body/>
     </div>
   )
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './header.css';
+import {useState} from 'react';
 
 export default function Header(props) {
+     const [search, setSearch] = useState("");
     return(
         <div className="Header__Block">
             <h1 className="Header__Title">Stocks Dashboard</h1>
-            <input className="Header__Search" value={props.search} onChange={} />
+            <input className="Header__Search" value={search} onChange={e => setSearch(e.target.value)} />
         </div>
     )
 }
