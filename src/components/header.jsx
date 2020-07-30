f2cbbc290c6847e24129960acc8fd1ecbb1e3293
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-07-30 07:33:58 -0400

    can now start indexing through and setting state

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,6 +4,7 @@ import {useState} from 'react';
 
 export default function Header(props) {
      const [search, setSearch] = useState("");
+     const [open, setOpen] = useState([]);
     return(
         <div className="Header__Block">
             <h1 className="Header__Title">Stocks Dashboard</h1>
@@ -11,10 +12,11 @@ export default function Header(props) {
             <button onClick={() => fetch("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&outputsize=compact&apikey=90DCTUW9U71JIZB4")
   .then(response => response.json())
   .then(data => {
-    //   console.log(data["Time Series (Daily)"]);
+      const openArr = [];
       for (let week in data["Time Series (Daily)"]) {
-          console.log(data["Time Series (Daily)"][week]["1. open"])
+          openArr.push(data["Time Series (Daily)"][week]["1. open"])
       }
+      setOpen(openArr);
       })}>Search!</button>
         </div>
     )
